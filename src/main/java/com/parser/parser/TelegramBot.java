6e1d5eba4ad86c6ae48bf0ccc16765d37886dcf1
rshal <heiphin7and2@gmail.com>
2024-06-07 14:47:27 +0500

    Created basic comands in bot

--- src/main/java/com/parser/parser/TelegramBot.java
+++ src/main/java/com/parser/parser/TelegramBot.java
@@ -1,19 +1,32 @@
 package com.parser.parser;
 
-import org.springframework.stereotype.Component;
+import lombok.SneakyThrows;
+import org.telegram.telegrambots.bots.DefaultBotOptions;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 
-@Component
 public class TelegramBot extends TelegramLongPollingBot {
 
+    @SneakyThrows // for exception in sendApiMethod
     @Override
     public void onUpdateReceived(Update update) {
-        // todo something
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            var text = update.getMessage().getText();
+            var chatId = update.getMessage().getChatId();
+
+            SendMessage message = new SendMessage(chatId.toString(), text);
+            sendApiMethod(message);
+        }
     }
 
     @Override
     public String getBotUsername() {
         return "My-first-bot";
+    }
+
+    // Constructure for initialize bot
+    public TelegramBot(DefaultBotOptions options, String botToken) {
+        super(options, botToken);
     }
 }
